acc12cd756f2be913f2ed1310bdf47e86106a5b8
Eva Suissa <eva@cashback.co.il>
2020-11-02 16:06:44 +0200

    technique

--- src/MainFrench.js
+++ src/MainFrench.js
@@ -19,24 +19,23 @@ import Footer from "./components/Footer";
 import Marquee from "./components/Marquee";
 import IntervenantsFR from "./components/french/IntervenantsFR";
 import Commitee from "./components/french/Commitee";
+import Technique from "./components/french/Technique";
 import Inscription from "./components/Inscription";
 import EN from "./media/EN.png";
 import FR from "./media/FR.png";
 
 function MainFrench() {
   return (
-
     <div id="#MainFrench">
-        
-          <HomeFR />
-          <AboutFR />
-          <ProgrammeFR />
-          <Commitee/>
-          <IntervenantsFR />
-          <Marquee />
-          <Contact />
-          <Footer/>
-       
+      <HomeFR />
+      <AboutFR />
+      <ProgrammeFR />
+      <Commitee />
+      <IntervenantsFR />
+      <Technique />
+      <Marquee />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/french/Technique.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { TechniqueData } from "../../data/techniqueData";
+
+export default class Technique extends Component {
+  state = {
+    equipes: TechniqueData
+  };
+  render() {
+    let { equipes } = this.state;
+
+    return (
+      <div id="technique">
+        <div className="techniqueTitle">
+          <h1>Comité d'Organisation</h1>
+        </div>
+        <ul className="techniqueDisplay">
+          {equipes.map(equipe => {
+            return (
+              <li key={equipe.id}>
+                <img src={equipe.photo}></img>
+                <h3>{equipe.name}</h3>
+                <h5>{equipe.position}</h5>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
