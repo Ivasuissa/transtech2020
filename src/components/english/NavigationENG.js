7a2825664b0774e3096892f59c8802b5d22c9c68
Eva Suissa <eva@cashback.co.il>
2020-11-02 13:37:21 +0200

    event brite

--- src/components/english/NavigationENG.js
+++ src/components/english/NavigationENG.js
@@ -15,43 +15,67 @@ import Language from "../Language";
 
 const NavigationENG = props => {
   const handleChange = props.handleChange;
-  
+
   return (
     <div className="navigation">
-    <div className="header">
-      <div className="logo">
-        <h1> TRANSTECH 2020</h1>
-      </div>
-      <nav class="navbar navbar-expand-lg navbar-white bg-blue">
-      <div class="navbar-brand" id="button" onClick={() => handleChange()}>
-              FR
-           </div>
-        <a class="navbar-brand" target="_blank" href="http://transtech2020.eventbrite.com?s=125451659">Tickets</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#programme">Program</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#intervenants">Speakers</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact">Contact</a>
-            </li>
-          </ul>
+      <div className="header">
+        <div className="logo">
+          <h1> TRANSTECH 2020</h1>
         </div>
-</nav>
-</div>
-</div>
+        <nav class="navbar navbar-expand-lg navbar-white bg-blue">
+          <div class="navbar-brand" id="button" onClick={() => handleChange()}>
+            FR
+          </div>
+          <a
+            class="navbar-brand"
+            target="_blank"
+            href="https://www.eventbrite.com/e/transtech-2020-11th-edition-special-start-ups-paris-tlv-tickets-127696009201"
+          >
+            Tickets
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#home">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#programme">
+                  Program
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#intervenants">
+                  Speakers
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 };
 
