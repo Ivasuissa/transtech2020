e8cb55598db5241a4a69590c2af2b90e2572521e
Eva Suissa <eva@cashback.co.il>
2020-11-23 15:09:45 +0200

    ll

--- src/components/Inscription.js
+++ src/components/Inscription.js
@@ -11,19 +11,10 @@ const Inscription = () => {
           <a
             href="https://www.eventbrite.com/e/transtech-2020-11th-edition-special-start-ups-paris-tlv-tickets-127696009201"
             target="_blank"
-          >INSCRIPTION</a>
-         
-         </button>
-      </div>
-      <div className="inscriptionContent">
-        <button>
-          {" "}
-          <a
-            href="https://www.eventbrite.com/e/transtech-2020-11th-edition-special-start-ups-paris-tlv-tickets-127696009201"
-            target="_blank"
-          >INSCRIPTION</a>
-         
-         </button>
+          >
+            INSCRIPTION
+          </a>
+        </button>
       </div>
     </div>
   );
